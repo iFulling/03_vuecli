97cf7a0b044bf5971d273973afa4114ece44d9e5
Fulling <2435406817@qq,com>
2022-04-01 23:13:48 +0800

    测试3

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@
             About
           </router-link>
           <router-link class="list-group-item" active-class="active" to="/home">
-            Home12
+            Home123
           </router-link>
         </div>
       </div>
