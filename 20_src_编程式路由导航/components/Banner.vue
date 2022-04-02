049fca9de8f0875eb17a0514a59566a4668aca49
Fulling <2435406817@qq.com>
2022-04-02 18:32:57 +0800

    编程式路由导航

---
+++ 20_src_编程式路由导航/components/Banner.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="col-xs-offset-2 col-xs-8">
+    <div class="page-header"><h2>Vue Router Demo</h2></div>
+    <button @click="back">后退</button>
+    <button @click="forward">前进</button>
+    <button @click="backT">后退3步</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Banner",
+  methods: {
+    back() {
+      this.$router.back(); // 后退1步
+    },
+    forward() {
+      this.$router.forward(); // 前进1步
+    },
+    backT() {
+      this.$router.go(-3); // 负数为后退，正数为前进
+    },
+  },
+};
+</script>
+
+<style>
+</style>
