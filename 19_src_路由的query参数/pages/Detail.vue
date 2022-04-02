1d04cca8088d60001578c4105b2429d56d3a3c74
Fulling <2435406817@qq.com>
2022-04-02 14:09:26 +0800

    路由的query参数

---
+++ 19_src_路由的query参数/pages/Detail.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div>编号：{{ $route.query.id }}</div>
+    <div>信息：{{ $route.query.msg }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Detail",
+};
+</script>
+
+<style>
+</style>
